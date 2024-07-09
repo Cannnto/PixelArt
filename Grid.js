1b9b05b9d0727602910dcb1f26c701fe51b1d9a4
Cannnto <129905863+Cannnto@users.noreply.github.com>
2024-07-08 21:07:55 -0300

    Add files via upload

--- Grid.js
+++ Grid.js
@@ -3,16 +3,21 @@ class Grid
     {   this.rows = rows;
         this.coluns = coluns;
         this.blockSize = {width: Math.ceil(canvas.width/this.coluns), height: Math.ceil(canvas.height/this.rows)};
+
         this.map = [];
-        this.currentColor = null;
         this.colors = [];
-        this.count = 0;
-        for(let i=0;i<this.coluns*this.rows;i++) this.map[i] = 0;
+        this.realColors = [];
+        this.currentColor;
+        this.map[this.coluns*this.rows-1] = undefined;
     }
-    draw()
-    {   this.blockSize = {width: Math.ceil(canvas.width/this.coluns), height: Math.ceil(canvas.height/this.rows)};
+
+    draw(theme)
+    {   var color
+        if(!theme) color = 'rgba(255,255,255,0.05)';
+        else color = 'rgba(0,0,0,0.05)';
+
         for(let eachRow = 0; eachRow < this.rows; eachRow++)
-        {   ctx.strokeStyle = 'rgba(255,255,255,0.05)';
+        {   ctx.strokeStyle = color;
             ctx.beginPath();
                 ctx.moveTo(0, eachRow * this.blockSize.height);
                 ctx.lineTo(canvas.width, eachRow * this.blockSize.height);
@@ -20,7 +25,7 @@ class Grid
             ctx.stroke();
         }
         for(let eachColun = 0; eachColun < this.coluns; eachColun++)
-        {   ctx.strokeStyle = 'rgba(255,255,255,0.05)';
+        {   ctx.strokeStyle = color;
             ctx.beginPath();
                 ctx.moveTo(eachColun * this.blockSize.width, 0);
                 ctx.lineTo(eachColun * this.blockSize.width, canvas.height);
@@ -28,10 +33,10 @@ class Grid
             ctx.stroke();
         }
     }
-    updateMap()
-    {   this.map = [0]
-        for(let i = 0; i< this.coluns*this.rows; i++) this.map[i] = 0
 
+    updateMap()
+    {   this.map = []
+        this.map[this.coluns * this.rows-1] = undefined;
     }
 
     update(mouseX, mouseY, type)
@@ -42,16 +47,13 @@ class Grid
         {   for(let eachColun = 0; eachColun < this.coluns; eachColun++)
             {   let arrayIndex = eachRow * this.coluns + eachColun;
                 if(eachColun == colun-1 && eachRow == row-1)
-                {   if(type)    this.map[arrayIndex] = 0;
-                    else{    
-                        if(this.colors[this.colors.length-1] == this.currentColor)   this.map[arrayIndex] = this.colors[this.colors.length-2];
-                        else
-                        {   this.count++;
-                            this.colors.push(this.count);
-                            this.colors.push(this.currentColor);
-                            this.map[arrayIndex] = this.count
-                            ;
+                {   if(type)    this.map[arrayIndex] = undefined;
+                    else
+                    {   if(this.colors.indexOf(this.currentColor) == -1)
+                        {   this.colors.push(this.currentColor);
+                            this.realColors.push(`'${this.currentColor}'`);
                         }
+                        this.map[arrayIndex] = this.colors.indexOf(this.currentColor);
                     }
                 }
             }
@@ -59,12 +61,13 @@ class Grid
     }
     iterate(color)
     {   this.currentColor = color;
+        this.blockSize = {width: Math.ceil(canvas.width/this.coluns), height: Math.ceil(canvas.height/this.rows)};
+
         for(let eachRow = 0; eachRow < this.rows; eachRow++)
         {   for(let eachColun = 0; eachColun < this.coluns; eachColun++)
             {   let arrayIndex = eachRow * this.coluns + eachColun;
-                if(this.map[arrayIndex]){
-
-                    ctx.fillStyle = this.colors[this.colors.indexOf(this.map[arrayIndex])+1];
+                if(this.map[arrayIndex] >= 0)
+                {   ctx.fillStyle = this.colors[this.map[arrayIndex]];
                     ctx.fillRect(this.blockSize.width*eachColun, this.blockSize.height*eachRow, this.blockSize.width,this.blockSize.height);
                 }            
             }
